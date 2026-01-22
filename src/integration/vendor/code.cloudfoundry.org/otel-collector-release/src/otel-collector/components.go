8837ba1feff3b5b21e2dceb25bfae9c5fbc5995e
Agnello Noronha <agnello.noronha@broadcom.com>
2026-01-22 09:36:22 +0530

    Add opamp extension to otel-collector
    - Added opamp extension

--- src/integration/vendor/code.cloudfoundry.org/otel-collector-release/src/otel-collector/components.go
+++ src/integration/vendor/code.cloudfoundry.org/otel-collector-release/src/otel-collector/components.go
@@ -22,6 +22,7 @@ import (
 	datadogexporter "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/datadogexporter"
 	kafkaexporter "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/kafkaexporter"
 	syslogexporter "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/syslogexporter"
+	opampextension "github.com/open-telemetry/opentelemetry-collector-contrib/extension/opampextension"
 	pprofextension "github.com/open-telemetry/opentelemetry-collector-contrib/extension/pprofextension"
 	batchprocessor "go.opentelemetry.io/collector/processor/batchprocessor"
 	memorylimiterprocessor "go.opentelemetry.io/collector/processor/memorylimiterprocessor"
@@ -38,12 +39,14 @@ func components() (otelcol.Factories, error) {
 	}
 
 	factories.Extensions, err = otelcol.MakeFactoryMap[extension.Factory](
+		opampextension.NewFactory(),
 		pprofextension.NewFactory(),
 	)
 	if err != nil {
 		return otelcol.Factories{}, err
 	}
 	factories.ExtensionModules = make(map[component.Type]string, len(factories.Extensions))
+	factories.ExtensionModules[opampextension.NewFactory().Type()] = "github.com/open-telemetry/opentelemetry-collector-contrib/extension/opampextension v0.143.0"
 	factories.ExtensionModules[pprofextension.NewFactory().Type()] = "github.com/open-telemetry/opentelemetry-collector-contrib/extension/pprofextension v0.143.0"
 
 	factories.Receivers, err = otelcol.MakeFactoryMap[receiver.Factory](
